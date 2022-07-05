c102503915e413274a64dbb5e6b9c09f559e89d7
ko sai <saisaiant@gmail.com>
2022-07-05 14:30:02 +0630

    Add Home Page, Css & Layout

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
 import React from "react";
 
-const index = () => {
-  return <div>index</div>;
+import { Product, FooterBanner, HeroBanner } from "../components";
+
+const Home = () => {
+  return (
+    <>
+      <HeroBanner />
+      <div className="products-heading">
+        <h2>Best Selling Products</h2>
+        <p>Speakers of many variations</p>
+      </div>
+      <div className="products-container">
+        {["product1", "product2"].map((product) => product)}
+      </div>
+      <FooterBanner />
+    </>
+  );
 };
 
-export default index;
+export default Home;
